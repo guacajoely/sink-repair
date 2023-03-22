3d56ae3473f94798dc2d426a7fd6d66e42caac4d
guacajoely <guacajoely@gmail.com>
2023-03-22 11:26:56 -0400

    submit service request working

--- src/scripts/dataAccess.js
+++ src/scripts/dataAccess.js
@@ -20,3 +20,21 @@ export const fetchRequests = () => {
 export const getRequests = () => {
     return applicationState.requests.map(obj => ({...obj}))
 }
+
+
+export const sendRequest = (userServiceRequest) => {
+    const fetchOptions = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(userServiceRequest)
+    }
+
+
+    return fetch(`${API}/requests`, fetchOptions)
+        .then(response => response.json())
+        .then(() => {
+
+        })
+}
